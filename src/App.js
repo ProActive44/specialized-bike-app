08493f8b8ef7ef4b328b89f53bdc91bc42f33819
vinusingh36 <vineet.vinusingh36@gmail.com>
2023-05-25 21:35:09 +0530

    added login and signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import { Signup } from './Pages/Accountpage/Signup';
+import { Login } from './Pages/Accountpage/Login';
 
 function App() {
   return (
     <div className="App">
       {/*  use your component here don't make any changes in previous code  */}
-
-            
+      <Login />
+      <Signup />
     </div>
   );
 }
--- src/Pages/Accountpage/login.jsx
+++ src/Pages/Accountpage/login.jsx
@@ -1,5 +1,50 @@
-export const login = () => {
+import React from "react"
+import "./accountpage.css"
+import {
+    FormLabel,
+    Input,
+    Heading,
+    Checkbox,
+    Button,
+    CheckboxIcon
+}
+    from "@chakra-ui/react";
+
+export const Login = () => {
     return (
-        <div>Login page</div>
+        <div>
+            <div className="main_form_div">
+                <Heading fontWeight="10px" fontSize="32px">Sign in to your Account</Heading>
+                <br />
+                <form  >
+                    <FormLabel> Email </FormLabel>
+                    <Input type="email" placeholder="Email" required />
+                    <br />
+
+                    <FormLabel> Password </FormLabel>
+                    <Input type="password" placeholder="Password" required />
+                    <br />
+                    <br />
+                    <div className="item_display_corner">
+                        <div>
+                            <Checkbox  >Remember Me</Checkbox>
+                        </div>
+                        <div><a href="">Forgot your password?</a> </div>
+                    </div>
+
+
+                    <div className="item_center">
+                        <p className="small_font">
+                            I accept the Specialized <a href="https://www.specialized.com/sg/en/terms-of-use" className="hover_text_color">Terms of Use</a> and acknowledge Specialized will use my information in accordance with its <a href="https://www.specialized.com/sg/en/privacy-policy" className="hover_text_color">Privacy Policy.</a>
+                        </p>
+                    </div>
+                    <br />
+                    <Button type="submit" className="btn">  Sign In  </Button>
+                    <br />
+                    <br />
+                    <a href="" className="create_account_btn">Create Account</a>
+                </form>
+            </div>
+        </div>
     )
 }
