47c780ff882a273af42002beec22cd1af26ced56
vinusingh36 <vineet.vinusingh36@gmail.com>
2023-05-27 00:18:54 +0530

    redux store has been added

--- src/Pages/Accountpage/login.jsx
+++ src/Pages/Accountpage/login.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import "./accountpage.css"
 import {
     FormLabel,
@@ -6,23 +6,85 @@ import {
     Heading,
     Checkbox,
     Button,
-    CheckboxIcon
+    CheckboxIcon,
+    ButtonGroup,
+    InputGroup,
+    InputRightElement,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    useDisclosure
 }
     from "@chakra-ui/react";
+import { Signup } from "./SignupPage";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom"
+import { calcLength } from "framer-motion";
+import { getCurrentUser } from "../../Redux/action";
+
+
+
 
 export const Login = () => {
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const store = useSelector((store) => store.accountReducer.AllUsers)
+    const currUser = useSelector((store) => store.accountReducer.currUser);
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const [show, setShow] = useState(false)
+    const handleClick = () => setShow(!show)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        store.map((user) => {
+            if (email === user.email && password === user.password) {
+                alert("successful");
+                dispatch(getCurrentUser(user))
+                navigate('/Homepage')
+                return;
+            }
+        })
+        console.log(currUser);
+        alert("Wrong Credentails");
+
+    }
+
     return (
         <div>
             <div className="main_form_div">
                 <Heading fontWeight="10px" fontSize="32px">Sign in to your Account</Heading>
                 <br />
-                <form  >
+                <form onSubmit={handleSubmit}>
                     <FormLabel> Email </FormLabel>
-                    <Input type="email" placeholder="Email" required />
+                    <Input type="email" placeholder="Email" required onChange={(e) => setemail(e.target.value)} />
                     <br />
 
                     <FormLabel> Password </FormLabel>
-                    <Input type="password" placeholder="Password" required />
+                    <InputGroup size='md'>
+                        <Input
+                            type={show ? 'text' : 'password'}
+                            placeholder='Password'
+                            required
+                            onChange={(e) => setpassword(e.target.value)}
+                        />
+                        <InputRightElement width='4.5rem' >
+                            <Button h='1.75rem' size='sm' onClick={handleClick} >
+                                {show ? 'Hide' : 'Show'}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+
                     <br />
                     <br />
                     <div className="item_display_corner">
@@ -39,11 +101,24 @@ export const Login = () => {
                         </p>
                     </div>
                     <br />
-                    <Button type="submit" className="btn">  Sign In  </Button>
+                    <ButtonGroup variant='outline' width="100%" >
+                        <Button type="submit" className="btn" colorScheme="yellow">  Sign In  </Button>
+                    </ButtonGroup>
+
                     <br />
                     <br />
-                    <a href="" className="create_account_btn">Create Account</a>
+                    <ButtonGroup variant='outline' width="100%" >
+                        <Button onClick={onOpen} className="btn" colorScheme="yellow">Create Account</Button>
+                    </ButtonGroup>
                 </form>
+                <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={true}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalCloseButton />
+                        <Signup />
+                    </ModalContent>
+                </Modal>
+
             </div>
         </div>
     )
