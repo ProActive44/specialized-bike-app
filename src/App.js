cfb867e52a196d3b349fffb73f9981c5b4907289
vinusingh36 <vineet.vinusingh36@gmail.com>
2023-05-28 13:39:06 +0530

    Login page name changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { Login } from './Pages/Accountpage/Login';
+
 
 
 function App() {
   return (
     <div className="App">
       {/*  use your component here don't make any changes in previous code  */}
-      <Login />
+
     </div>
   );
 }
