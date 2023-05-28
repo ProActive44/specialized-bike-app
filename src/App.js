5c8c4972a820c18024aff5d4dfed71ed28d83086
Prasad <prasadmhaske2001@gmail.com>
2023-05-28 13:29:20 +0530

    Homepage Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
+import Homepage from './Pages/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
       {/*  use your component here don't make any changes in previous code  */}
 
-            
+            <Navbar/>
+            <Homepage/>
+            <Footer/>
     </div>
   );
 }
