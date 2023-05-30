02f65e31861fcc27396f5a8d98a2106f15560a7e
vinusingh36 <vineet.vinusingh36@gmail.com>
2023-05-30 22:01:43 +0530

    added new code

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "../Pages/Homepage/Homepage";
 import { Login } from "../Pages/Accountpage/LoginPage";
 import { Signup } from "../Pages/Accountpage/SignupPage";
+import ProductPage from "../Pages/Productpage/ProductPage"
 
 const AllRoutes = () => {
   return (
@@ -10,7 +11,6 @@ const AllRoutes = () => {
       <Route path="/" element={<Homepage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/signupsuccess" element={<SignupSuccess />} />
       <Route path="/productPage" element={<ProductPage />} />
       <Route path="*" element={<Homepage />} />
     </Routes>
