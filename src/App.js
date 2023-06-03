e6bff251c01607beee0c07638fcf48e277d284e0
VanshikaAgarwal2508 <rkagarwal313@gmail.com>
2023-06-03 14:08:00 +0530

    payment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+//import CartPage from "./Pages/Cartpage/cart"
+import PaymentPage from './Pages/Checkoutpage/payment';
 
 function App() {
   return (
     <div className="App">
       {/*  use your component here don't make any changes in previous code  */}
-
-            
+     
+     <PaymentPage/>
     </div>
   );
 }
