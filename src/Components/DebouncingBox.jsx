916545dd8e64d58ccb15c3cd2e7a763a1619548c
Prasad <prasadmhaske2001@gmail.com>
2023-09-27 22:33:02 +0530

    debouncing edited

--- src/Components/DebouncingBox.jsx
+++ src/Components/DebouncingBox.jsx
@@ -8,30 +8,26 @@ import {
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const DebouncingBox = ({ isOpen, onClose, debouncingProducts }) => {
+const DebouncingBox = ({ debouncingProducts }) => {
   const navigate = useNavigate();
-  console.log(debouncingProducts);
+  // console.log(debouncingProducts);
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          {debouncingProducts?.map((ele) => {
-            return (
-              <div key={ele._id}>
-                <Text
-                  _hover={{ cursor: "pointer" }}
-                  my={"5px"}
-                  onClick={() => navigate(`/productPage/details/${ele._id}`)}
-                >
-                  {ele.name}
-                </Text>
-                <Divider />
-              </div>
-            );
-          })}
-        </ModalContent>
-      </Modal>
+      {debouncingProducts?.map((ele) => {
+        return (
+          <div key={ele._id}>
+            <Text
+              _hover={{ cursor: "pointer", color: "red" }}
+              color={"yellow"}
+              my={"5px"}
+              onClick={() => navigate(`/productPage/details/${ele._id}`)}
+            >
+              {ele.name}
+            </Text>
+            <Divider />
+          </div>
+        );
+      })}
     </>
   );
 };
